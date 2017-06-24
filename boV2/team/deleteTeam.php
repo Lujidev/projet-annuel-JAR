949f7d3412aa7dd9ca384dba13c3a80f89e4544c
Lujidev <thelinjing@gmail.com>
2017-06-24 11:16:48 +0200

    Fixe manageTeam.php Update: + Quitter une equipe + Le createur est mtn automatiquement ajouté dans son groupe. Modification saveTeam

--- boV2/team/deleteTeam.php
+++ boV2/team/deleteTeam.php
@@ -3,23 +3,49 @@ require "../lib.php";
 require "libTeam.php";
 
 $id_team = $_GET["id"];
+$id_user = $_GET['userId'];
 
 $db = dbConnect();
 
-$query = $db->prepare("DELETE FROM PROJETS WHERE id_team=:id");
-$query->execute([
-    "id"=>$id_team
+$queryCheck = $db->prepare("SELECT id FROM EQUIPES WHERE id=:id AND createur=:userId");
+
+$queryCheck->execute([
+    "id"=>$id_team,
+    "userId"=>$id_user
 ]);
 
-$query2 = $db->prepare("DELETE FROM TEAMMATES WHERE id_team=:id");
-$query2->execute([
-    "id"=>$id_team
-]);
+$ifUserIsCreator = $queryCheck->fetch();
 
-$query1 = $db->prepare("DELETE FROM EQUIPES WHERE id=:id");
-$query1->execute([
-    "id"=>$id_team
-]);
+if (!empty($ifUserIsCreator)){
 
-header("location: ../manageTeam.php");
+    $query = $db->prepare("DELETE FROM PROJETS WHERE id_team=:id");
+    $query->execute([
+        "id"=>$id_team
+    ]);
+
+    $query2 = $db->prepare("DELETE FROM TEAMMATES WHERE id_team=:id");
+    $query2->execute([
+        "id"=>$id_team
+    ]);
+
+    $query1 = $db->prepare("DELETE FROM EQUIPES WHERE id=:id");
+    $query1->execute([
+        "id"=>$id_team
+    ]);
+
+    header("location: ../manageTeam.php");
+
+}else{
+
+    $query2 = $db->prepare("DELETE FROM TEAMMATES WHERE id_user=:id_user AND id_team = :id_team");
+    $query2->execute([
+        "id_user"=>$id_user,
+        "id_team"=>$id_team
+    ]);
+
+    header("location: ../manageTeam.php");
+
+}
+
+
 ?>
